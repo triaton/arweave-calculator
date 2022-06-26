0543ffd2960c929ebf48c9e829d16916e2bb4a55
webcat12345 <rango0986@gmail.com>
2022-06-26 05:28:09 -0700

    feat: update tailwindcss config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { HomeModule } from './home/home.module';
 import { AppComponent } from './app.component';
 
